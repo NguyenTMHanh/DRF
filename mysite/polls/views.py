f221cf4fef0d016e581e7b65c9448f412e60589e
My Hanh <myhanh13022002@gmail.com>
2024-10-01 00:25:01 +0700

    done lab1/part3

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 
-
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
-
+from .models import Question
+from django.template import loader
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+
+def results(request, question_id):
+    response = "bạn đang nhìn thấy kết quả của câu hỏi số %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("bạn đã bình chọn cho câu hỏi số %s." % question_id)
